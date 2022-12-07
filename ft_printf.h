9348401553fbbe436d4c6c1e12b04ae60faef477
Lars Bonnefoy <lbonnefo@fu-r1-p8.s19.be>
2022-12-07 09:22:01 +0100

    working with libft

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: lbonnefo <lbonnefo@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 16:07:24 by lbonnefo          #+#    #+#             */
-/*   Updated: 2022/10/22 12:15:18 by lbonnefo         ###   ########.fr       */
+/*   Updated: 2022/12/07 09:16:20 by lbonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 # include <stdlib.h>
 # include <stdarg.h>
 
-void	ft_putchar_fd(char c, int fd, int *rtn);
-size_t	ft_strlen(const char *s);
+void	ft_putchar_fd_printf(char c, int fd, int *rtn);
+size_t	ft_strlen_printf(const char *s);
 void	ft_is_hex(unsigned int nbr, char *base, int fd, int *rtn);
 void	ft_is_str(char *s, int fd, int *rtn);
 void	ft_is_unsigned(unsigned int unb, int fd, int *rtn);
